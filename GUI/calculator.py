49c84029e75c7f1a2985f23cdbd9b2c10fdf612e
KhengTang <khengtangjack@gmail.com>
2020-07-09 14:19:39 +0800

    Add history input

--- GUI/calculator.py
+++ GUI/calculator.py
@@ -1,9 +1,14 @@
 from tkinter import *
 
+historyStr = []
+
 window = Tk()
 
+inputHistory = Label(window, text='')
+inputHistory.grid(row=0, column=0, columnspan=3, padx=10)
+
 userInput = Entry(window)
-userInput.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+userInput.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
 
 def button_click(number):
   #userInput.delete(0, END)
@@ -13,37 +18,32 @@ def button_click(number):
   
 def button_clear():
   userInput.delete(0, END)
+  global historyStr
+  historyStr.clear()
+  inputHistory.destroy()
   
-def button_add():
+def button_sign(logic):
   first = userInput.get()
   global f_num
   global math
-  math = "add"
+  global historyStr
+  sign = ""
+  if logic == "add":
+    math = "add"
+    sign = " + "
+  if logic == "sub":
+    math = "sub"
+    sign = " - "
+  if logic == "mul":
+    math = "mul"
+    sign = " * "
+  if logic == "div":
+    math = "div"
+    sign = " / "
   f_num = int(first)
-  userInput.delete(0, END)
-
-def button_sub():
-  first = userInput.get()
-  global f_num
-  global math
-  math = "sub"
-  f_num = int(first)
-  userInput.delete(0, END)
-  
-def button_mul():
-  first = userInput.get()
-  global f_num
-  global math
-  math = "mul"
-  f_num = int(first)
-  userInput.delete(0, END)
-  
-def button_div():
-  first = userInput.get()
-  global f_num
-  global math
-  math = "div"
-  f_num = int(first)
+  historyStr.append(first + sign)
+  inputHistory = Label(window, text=''.join(map(str, historyStr)))
+  inputHistory.grid(row=0, column=0, columnspan=3, padx=10)
   userInput.delete(0, END)
   
 def button_equal():
@@ -69,33 +69,33 @@ btn_7 = Button(window, text="7", padx=40, pady=20, command=lambda: button_click(
 btn_8 = Button(window, text="8", padx=40, pady=20, command=lambda: button_click(8))
 btn_9 = Button(window, text="9", padx=40, pady=20, command=lambda: button_click(9))
 btn_0 = Button(window, text="0", padx=40, pady=20, command=lambda: button_click(0))
-btn_add = Button(window, text="+", padx=39, pady=20, command=button_add)
+btn_add = Button(window, text="+", padx=39, pady=20, command=lambda: button_sign("add"))
 btn_equal = Button(window, text="=", padx=91, pady=20, command=button_equal)
 btn_clear = Button(window, text="Clear", padx=79, pady=20, command=button_clear)
-btn_subtract = Button(window, text="-", padx=40, pady=20, command=button_sub)
-btn_multiply = Button(window, text="*", padx=40, pady=20, command=button_mul)
-btn_divide = Button(window, text="/", padx=40, pady=20, command=button_div)
+btn_subtract = Button(window, text="-", padx=40, pady=20, command=lambda: button_sign("sub"))
+btn_multiply = Button(window, text="*", padx=40, pady=20, command=lambda: button_sign("mul"))
+btn_divide = Button(window, text="/", padx=40, pady=20, command=lambda: button_sign("div"))
 # Put button on screen
 
-btn_1.grid(row=3, column=0)
-btn_2.grid(row=3, column=1)
-btn_3.grid(row=3, column=2)
+btn_1.grid(row=4, column=0)
+btn_2.grid(row=4, column=1)
+btn_3.grid(row=4, column=2)
 
-btn_4.grid(row=2, column=0)
-btn_5.grid(row=2, column=1)
-btn_6.grid(row=2, column=2)
+btn_4.grid(row=3, column=0)
+btn_5.grid(row=3, column=1)
+btn_6.grid(row=3, column=2)
 
-btn_7.grid(row=1, column=0)
-btn_8.grid(row=1, column=1)
-btn_9.grid(row=1, column=2)
+btn_7.grid(row=2, column=0)
+btn_8.grid(row=2, column=1)
+btn_9.grid(row=2, column=2)
 
-btn_0.grid(row=4, column=0)
-btn_clear.grid(row=4,column=1, columnspan=2)
-btn_add.grid(row=5,column=0)
-btn_equal.grid(row=5,column=1, columnspan=2)
+btn_0.grid(row=5, column=0)
+btn_clear.grid(row=5,column=1, columnspan=2)
+btn_add.grid(row=6,column=0)
+btn_equal.grid(row=6,column=1, columnspan=2)
 
-btn_subtract.grid(row=6,column=0)
-btn_multiply.grid(row=6,column=1)
-btn_divide.grid(row=6,column=2)
+btn_subtract.grid(row=7,column=0)
+btn_multiply.grid(row=7,column=1)
+btn_divide.grid(row=7,column=2)
 
 window.mainloop()
